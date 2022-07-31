1fd30ea28f42ec22c9042e753937cbf4992424f2
Jason <jclumpkin1s@gmail.com>
2022-07-31 14:02:27 -0500

    Update api_client.py

--- api_client.py
+++ api_client.py
@@ -28,7 +28,7 @@ class ApiClient():
         
         json_str=json.dumps(self.batched_data,indent=4)
 
-        res = requests.get(url, headers=headers, auth=self.auth, json=json_str)
+        res = requests.put(url, headers=headers, auth=self.auth, json=json_str)
         print(res.text)
 
 
